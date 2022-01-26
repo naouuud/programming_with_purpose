6aede4f5b1709e7d677f14bf722a2944137528d8
naouuud <95048187+naouuud@users.noreply.github.com>
2022-01-26 17:07:57 +0200

    Update BirthdayAlt.java

--- Week 3 Arrays/BirthdayAlt.java
+++ Week 3 Arrays/BirthdayAlt.java
@@ -9,7 +9,7 @@
  *  Coursera User ID:  123456
  *  Last modified:     October 16, 1842
  **************************************************************************** */
-
+// This is a failed attempt at the problem using for loops, keeping it here for reference
 public class BirthdayAlt {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
